4dc2fc24c9a72bae5ab5806580ea712e6eface3c
ishach003 <choudharyisha2003@gmail.com>
2026-04-04 10:35:28 +0530

    Initial Commit

---
+++ src/test/java/com/foodDeliveryFrontend/FoodDeliveryFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodDeliveryFrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
