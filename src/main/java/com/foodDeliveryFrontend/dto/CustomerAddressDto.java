87b37780947236a8409cb588a18271de020b5703
Harshita <harshitagahlot05@gmail.com>
2026-04-04 10:54:33 +0530

    Dto created for CustomerAddressDto

---
+++ src/main/java/com/foodDeliveryFrontend/dto/CustomerAddressDto.java
@@ -0,0 +1,75 @@
+package com.foodDeliveryFrontend.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CustomerAddressDto {
+
+    private Integer addressId;
+
+    @JsonProperty("addressLine1")
+    private String street;
+
+    @JsonProperty("addressLine2")
+    private String addressLine2;
+
+    private String city;
+
+    public Integer getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(Integer addressId) {
+        this.addressId = addressId;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    private String state;
+
+    @JsonProperty("postalCode")
+    private String zipCode;
+
+}
+
+
+
